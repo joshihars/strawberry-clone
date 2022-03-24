2ac17dce14dafd2fba2e00411b83d1f2145eb38e
Harshit Joshi <93377385+joshihars@users.noreply.github.com>
2022-03-24 14:23:07 +0530

    Add files via upload

---
+++ cartFlagModalPlusForm.js
@@ -0,0 +1,38 @@
+function cartFlagModalPlusForm()
+{
+    return `<div id="megaContainer">
+    <div id="shopping_bag_title">
+        SHOPPING  BAG    
+    </div>
+
+    <div id="deliverToAndForm">
+
+        <div id="deliverTo">
+            <div id="deliverToText">
+                <span id="flagModalTrigger">
+                DELIVER TO: 
+                
+                    <img src="https://upload.wikimedia.org/wikipedia/en/4/41/Flag_of_India.svg">
+                    India
+                    
+                    <i class="fa fa-arrow-right"></i>
+
+                </span>
+            </div>
+             
+        </div>
+
+
+    </div>
+
+    <div id="flagModal">
+        <div id="flagModalContent">
+
+        </div>
+    </div>
+
+</div>
+    `
+}
+
+export default cartFlagModalPlusForm();
---
+++ navbar.js
@@ -0,0 +1,52 @@
+function navbar()
+{
+    return `<div id="cont">
+        <img src="https://i.ibb.co/TYXhCMm/Logo.jpg">
+    
+        <div>
+            <span>
+                <input type="text" placeholder="SEARCH BRAND/PRODUCT" id="searchBar">
+                <button id="searchButton"><i class="fa fa-search" id="searchButtonIcon"></i></button>
+            </span>
+            <div id="textBelowSearchBar">
+                <span>
+                    Natural Beauty<span id="textBelowSearchBar_Divider"></span>Estee Lauder<span
+                        id="textBelowSearchBar_Divider"></span>Shiseido
+                    <span id="textBelowSearchBar_Divider"></span>Double Serum
+                </span>
+                  <div id="search_div"></div>
+            </div>
+        </div>
+    
+        <span style="display: inline-flex;" id="threeIconsInNavBar">
+            <div id="signin_icon">
+                <i class="fa fa-user-circle" id="signinicon"></i>
+                <div id="userprofile"></div>
+            </div>
+            <div id="wishlist_icon">
+                <i class="fa fa-heart" id="wishlist_icon"></i>
+            </div>
+            <div id="bag_icon">
+                <i class="fa fa-shopping-bag" id="bag_icon"></i>
+            </div>
+        </span>
+    
+    
+    </div>
+    
+    <nav id="navbar">
+        <div class="navMenu"><a href="#">SHOP BY BRAND</a></div>
+        <div class="navMenu"> <a href="#">SKINCARE</a></div>
+        <div class="navMenu"><a href="#">MAKEUP</a></div>
+        <div class="navMenu"><a href="#">HAIRCARE</a></div>
+        <div class="navMenu"><a href="#">PERFUME</a></div>
+        <div class="navMenu"><a href="#">MEN'S SKINCARE</a></div>
+        <div class="navMenu"><a href="#">MEN'S COLOGNE</a></div>
+        <div class="navMenu"><a href="#">HOME SCENTS</a></div>
+        <div class="navMenu"><a href="#">NATURAL BEAUTY</a></div>
+        <div class="navMenu"><a href="#">SPECIALS</a></div>
+        <div class="navMenu"><a href="#"><i class="fa fa-star"></i>NEW</a></div>
+    </nav>`;
+}
+
+export  {navbar};
